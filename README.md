3a3c9ffc482512ac86c5c115a59a4c0617f99039
Sander Verhagen <sander@sanderverhagen.net>
2018-01-13 18:38:33 -0800

    master: Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# from-toggl
+Simple scripts to convert [Toggl](https://toggl.com/) time entries into another format. As a contractor, I track my time
+in Toggl, and it's easy to export time sheets/reports, but some customers desire hours to be entered in a different
+format or a different application.
+
+Some features:
+- Merge time entries when these are essentially the same block of work
+- Format for _Kronos_
+
+
+## Requirements
+
+Python 3.6 is required.
+
+## Installation
+
+1. Clone this repository
+2. Preferably create a virtual environment, but in any case install the dependencies from `requirements.txt`. E.g.:
+
+       mkvirtualenv from-toggl -p python3.6 -r requirements.txt
+
+3. [Get an API token from Toggl](https://toggl.com/app/profile)
+4. Set the API token as the `TOGGL_API_TOKEN` environment variable. E.g.:
+
+       export TOGGL_API_TOKEN=" 1234567890abcdef1234567890abcdef"
+
+5. Configure the script as desired. See the `##### CONFIGURATION #####` block in `fromtoggl.py`. Set constants:
+
+   - `CLIENT_ID`: number signifying the client ID
+   - `GAP`: maximum time between two time entries to consider these the same block of work and merge them together  
+   - `START_DATE`: start date to export time entries from 
+
+6. Run the script:
+
+       python fromtoggl.py
+
+7. Enjoy the output:
+
+                 Actual  Actual  Actual  Punch
+       Day       In Date In      Out     Hours
+       --------- ------- ------- ------- -----
+       Monday    1/08/18 100P    350P    2.83
+       Tuesday   1/09/18 900A    1145A   2.75
+       Tuesday   1/09/18 130P    451P    3.35
+       Wednesday 1/10/18 900A    1203P   3.05
+       Wednesday 1/10/18 1249P   600P    5.18
+       Thursday  1/11/18 910A    1155A   2.75
+       Thursday  1/11/18 105P    600P    4.92
+       Friday    1/12/18 900A    1200P   3.00
+       Friday    1/12/18 113P    500P    3.78
+       Friday    1/12/18 958P    124A    3.43
+       Saturday  1/13/18 135A    148A    .22
---
+++ fromtoggl.py
@@ -0,0 +1,146 @@
+import logging
+import os
+from datetime import date, timedelta
+
+import dateutil.parser
+import requests
+from tzlocal import get_localzone
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__package__)
+
+# ##### CONFIGURATION START #####
+CLIENT_ID = 34710800
+GAP = timedelta(minutes=10)
+START_DATE = date(2018, 1, 8)
+
+# ##### CONFIGURATION END #####
+
+ENVIRONMENT_VARIABLE_TOGGL_API_TOKEN = "TOGGL_API_TOKEN"
+
+
+def get_api_token():
+    if not ENVIRONMENT_VARIABLE_TOGGL_API_TOKEN in os.environ:
+        raise EnvironmentError("missing {} environment variable, which should contain your Toggl API token".format(
+            ENVIRONMENT_VARIABLE_TOGGL_API_TOKEN))
+
+    return os.environ[ENVIRONMENT_VARIABLE_TOGGL_API_TOKEN]
+
+
+auth = (get_api_token(), "api_token")
+
+response = requests.get(
+    "https://www.toggl.com/api/v8/clients/{}/projects".format(CLIENT_ID),
+    auth=auth
+)
+
+data = response.json()
+pids = [entry["id"] for entry in data]
+logger.info("processing projects: {}".format(pids))
+
+start_date_string = "{}T00%3A00%3A00%2B00%3A00".format(START_DATE.isoformat())
+
+response = requests.get(
+    "https://www.toggl.com/api/v8/time_entries?start_date={}".format(start_date_string),
+    auth=auth
+)
+
+data = response.json()
+
+results = []
+previous_start = None
+previous_stop = None
+
+
+def append_result():
+    if previous_start:
+        results.append({
+            "start": previous_start,
+            "stop": previous_stop
+        })
+
+
+for entry in data:
+    pid = entry["pid"]
+    if not entry["pid"] in pids:
+        logger.info("skipping project {}".format(pid))
+        continue
+
+    start = dateutil.parser.parse(entry["start"])
+    stop = dateutil.parser.parse(entry["stop"])
+
+    delta = start - previous_stop if previous_stop else None
+    new_entry = not previous_stop or (delta > GAP if delta else None)
+
+    logger.debug("previous stop: {}, start: {}, stop: {}, gap: {}, is new entry: {}".format(
+        previous_stop.astimezone(get_localzone()) if previous_stop else None,
+        start.astimezone(get_localzone()),
+        stop.astimezone(get_localzone()),
+        delta,
+        new_entry
+    ))
+
+    if new_entry:
+        append_result()
+
+        previous_start = start
+
+    previous_stop = stop
+
+append_result()
+
+
+def format_datetime(date_time):
+    if date_time:
+        local_date_time = date_time.astimezone(get_localzone())
+        date = local_date_time.strftime("%m/%d/%y").lstrip("0")
+        return "{} {}".format(date, format_time(date_time))
+
+    return "-"
+
+
+def format_weekday(date_time):
+    if date_time:
+        local_date_time = date_time.astimezone(get_localzone())
+        return local_date_time.strftime("%A").ljust(9)
+
+    return "-"
+
+
+def format_time(date_time):
+    if date_time:
+        local_date_time = date_time.astimezone(get_localzone())
+        am_pm = local_date_time.strftime("%p").upper()[0]
+        time = local_date_time.strftime("%I%M").lstrip("0")
+        return "{}{}".format(time, am_pm)
+
+    return "-"
+
+
+def format_delta(start, stop):
+    if not start or not stop:
+        return "-"
+
+    truncated_start = start.replace(second=0, microsecond=0)
+    truncated_stop = stop.replace(second=0, microsecond=0)
+    delta = truncated_stop - truncated_start
+
+    minutes, seconds = divmod(delta.seconds, 60)
+    decimal = minutes / 60
+    return "{:.2f}".format(decimal).lstrip("0")
+
+
+print()
+print("                Actual  Actual  Actual  Punch")
+print("Day             In Date In      Out     Hours")
+print("--------------- ------- ------- ------- -----")
+for result in results:
+    start = result["start"]
+    stop = result["stop"]
+    delta = stop - start
+    print("{}\t{}\t{}\t{}".format(
+        format_weekday(start),
+        format_datetime(start),
+        format_time(stop),
+        format_delta(start, stop)
+    ))
